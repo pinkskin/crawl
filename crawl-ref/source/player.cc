85290c25ca4aff597b0d42f6d204e87b747c8db9
Edgar A. Bering IV <trizor@gmail.com>
2021-08-12 22:01:14 +0300

    feat: persistent hell effects: rust dis
    The first of four branch-wide hell effects, meant to give each hell a
    unique tactical experience. In Dis, apply two stacks of corrosion.

--- crawl-ref/source/player.cc
+++ crawl-ref/source/player.cc
@@ -5931,6 +5931,9 @@ int player::corrosion_amount() const
     if (duration[DUR_CORROSION])
         corrosion += you.props[CORROSION_KEY].get_int();
 
+    if (player_in_branch(BRANCH_DIS))
+        corrosion += 2;
+
     return corrosion;
 }
 
