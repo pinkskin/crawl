f77364cd2374e683599266754c2661cafaa7638d
Nicholas Feinberg <pleasingfung@gmail.com>
2021-08-21 14:47:18 -0700

    Checkwhite

--- crawl-ref/source/webserver/game_data/static/cell_renderer.js
+++ crawl-ref/source/webserver/game_data/static/cell_renderer.js
@@ -1040,7 +1040,7 @@ function ($, view_data, main, tileinfo_player, icons, dngn, enums,
                 this.draw_icon(icons.SLOWLY_DYING, x, y, -status_shift, 0, img_scale);
                 status_shift += 10;
             }
-	    if (fg.FIRE_CHAMP)
+            if (fg.FIRE_CHAMP)
             {
                 this.draw_icon(icons.FIRE_CHAMP, x, y, -status_shift, 0, img_scale);
                 status_shift += 7;
